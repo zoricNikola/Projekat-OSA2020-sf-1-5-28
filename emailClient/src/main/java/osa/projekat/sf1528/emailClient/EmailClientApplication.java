e246d43fd63a0521bd525f876d89e9fc2aa1d0a9
zoricNikola <nikola.se.zoric@gmail.com>
2020-06-16 23:14:29 +0200

    Initial commit

---
+++ emailClient/src/main/java/osa/projekat/sf1528/emailClient/EmailClientApplication.java
@@ -0,0 +1,13 @@
+package osa.projekat.sf1528.emailClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailClientApplication.class, args);
+	}
+
+}
---
+++ emailClient/src/test/java/osa/projekat/sf1528/emailClient/EmailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package osa.projekat.sf1528.emailClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
