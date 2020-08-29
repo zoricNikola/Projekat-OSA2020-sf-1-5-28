b7c899d54e05126dcf210b0e4958255db69d38a4
zoricNikola <nikola.se.zoric@gmail.com>
2020-08-30 00:24:09 +0200

    Poboljsan login sistem

--- emailClient/src/main/java/osa/projekat/sf1528/emailClient/controller/AuthenticationController.java
+++ emailClient/src/main/java/osa/projekat/sf1528/emailClient/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,7 +24,7 @@ import osa.projekat.sf1528.emailClient.service.UserService;
 import osa.projekat.sf1528.emailClient.util.TokenUtil;
 
 @RestController
-@RequestMapping(value = "/auth")
+@RequestMapping(value = "api/auth")
 public class AuthenticationController {
 
 	@Autowired
@@ -45,16 +46,24 @@ public class AuthenticationController {
 	public ResponseEntity<TokenDTO> userLogin(@RequestBody LoginDTO loginDTO) {
 		
 		String username = loginDTO.getUsername();
+		User user = userService.findByUsername(username);
 		
-		Authentication authentication = authenticationManager.
-				authenticate(new UsernamePasswordAuthenticationToken(username, loginDTO.getPassword()));
+		if (user == null)
+			return new ResponseEntity<TokenDTO>(HttpStatus.NOT_FOUND);
 		
-		SecurityContextHolder.getContext().setAuthentication(authentication);
+		try {
+			Authentication authentication = authenticationManager.
+					authenticate(new UsernamePasswordAuthenticationToken(username, loginDTO.getPassword()));
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} catch (BadCredentialsException e) {
+			return new ResponseEntity<TokenDTO>(HttpStatus.UNAUTHORIZED);
+		} catch(Exception e) {
+			return new ResponseEntity<TokenDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		
 		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 		String token = tokenUtil.generateToken(userDetails);
 		
-		User user = userService.findByUsername(username);
 		user.setPassword(null);
 		
 		return new ResponseEntity<TokenDTO>(new TokenDTO(token, new UserDTO(user)), HttpStatus.OK);
